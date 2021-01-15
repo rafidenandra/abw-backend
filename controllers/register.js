a29fd0848bc8014334e59520c2f9c3808d3fe435
rafidenandra <rafiden01@gmail.com>
2021-01-15 12:57:49 +0700

    Edit register.js

--- controllers/register.js
+++ controllers/register.js
@@ -33,29 +33,6 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         })
         .catch(err => res.status(400).json('Unable to register!'))
 
-    // db.transaction(trx => {
-    //     trx("users")
-    //     .insert({
-    //         first_name: first_name,
-    //         last_name: last_name,
-    //         email: loginEmail[0],
-    //         institution: institution,
-    //         joined: new Date()
-    //     })
-    //     .returning("*")
-    //     .then(user => {
-    //         res.json(user[0]);
-    //         return trx("login")
-    //         .insert({
-    //             hash: hash, 
-    //             user_id: user[0].id
-    //         })
-    //         .returning("user_id");
-    //     })	
-    //     .then(trx.commit)
-    //     .catch(trx.rollback)
-    // })
-    // .catch( err => res.status(400).json("Unable to register!"));
 }
 
 module.exports = {
